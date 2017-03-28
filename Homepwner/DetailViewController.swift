b8ceaad54d85615a1dc15f612ba9b9c2fa3076ca
Sean Melnick Kelly <seanmelnickkelly@seans-macbook-pro.local>
2017-03-28 00:36:09 -0400

    Complete up to page 236

---
+++ Homepwner/DetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DetailViewController.swift
+//  Homepwner
+//
+//  Created by Sean Melnick Kelly on 3/28/17.
+//  Copyright © 2017 Sean Melnick Kelly. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+    
+    var item: Item!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        nameField.text = item.name
+        serialNumberField.text = item.serialNumber
+        valueField.text = "\(item.valueInDollars)"
+        dateLabel.text = "\(item.dateCreated)"
+    }
+}
